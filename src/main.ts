13e69a8eb9871e902d674a6acbee9da1d0cfcd90
rohit <rohit.rew@gmail.com>
2023-01-09 01:51:59 +0530

    cors settings changed

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import cors from "cors"
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
+
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-});
-  app.use(cors())
+    allowedHeaders: "*",
+    origin: "*",
+  });
+  app.use(cors());
   app.use(cookieParser());
   await app.listen(process.env.PORT || 4000);
 }
