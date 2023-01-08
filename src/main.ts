394f757b2f3f8e2ab4e4f2ad9ba110f3a0881604
rohit <rohit.rew@gmail.com>
2023-01-09 01:47:45 +0530

    cors settings changed

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   app.use(cors())
   app.use(cookieParser());
   await app.listen(process.env.PORT || 4000);
